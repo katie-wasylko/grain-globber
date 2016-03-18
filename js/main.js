8a44424b7a64f5f9f7501629d5ec1e70c03c742a
katie-wasylko <kwasylko@hotmail.com>
2016-03-18 11:03:32 -0400

    refigured main js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-var $ul = $('ul')
-
-grains.forEach( {
+grains.forEach(function (item) {
   var $li = $('<li>');
+  var $ul = $('ul');
+  var $p = $('<p>');
   var $img = $('<img>');
   var $h2 = $('<h2>');
-  var $p = $('<p>');
 
-  $li.append($img, $h2, $p)
-
-  $ul.append($li)
-}
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
+});
